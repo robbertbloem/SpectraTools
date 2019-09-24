77136508106b188a426c1d77536e9325b476e0df
Robbert Bloem <robbertbloem@tno.nl>
2019-09-24 09:49:11 +0200

    rearranged tests

--- Tests/Test_All.py
+++ Tests/Test_All.py
@@ -31,40 +31,40 @@ verbosity = 2
 TS = unittest.TestSuite()
 
 # TL = unittest.TestLoader()
-# tests = TL.loadTestsFromModule(SpectraTools.Tests.CommonFunctions_ST_Tests)
-# TS.addTests(tests)
-
-# TL = unittest.TestLoader()
 # tests = TL.loadTestsFromModule(SpectraTools.Tests.hitran_Tests)
 # TS.addTests(tests)
 
-# TL = unittest.TestLoader()
-# tests = TL.loadTestsFromModule(SpectraTools.Tests.LinearSpectrum_Tests)
-# TS.addTests(tests)
+TL = unittest.TestLoader()
+tests = TL.loadTestsFromModule(SpectraTools.Tests.CommonFunctions_ST_Tests)
+TS.addTests(tests)
+
+TL = unittest.TestLoader()
+tests = TL.loadTestsFromModule(SpectraTools.Tests.LinearSpectrum_Tests)
+TS.addTests(tests)
 
 TL = unittest.TestLoader()
 tests = TL.loadTestsFromModule(SpectraTools.Tests.MultiLinearSpectra_Tests)
 TS.addTests(tests)
 
-# TL = unittest.TestLoader()
-# tests = TL.loadTestsFromModule(SpectraTools.Tests.nist_import_jcamp_Tests)
-# TS.addTests(tests)
+TL = unittest.TestLoader()
+tests = TL.loadTestsFromModule(SpectraTools.Tests.nist_import_jcamp_Tests)
+TS.addTests(tests)
 
-# TL = unittest.TestLoader()
-# tests = TL.loadTestsFromModule(SpectraTools.Tests.nist_Tests)
-# TS.addTests(tests)
+TL = unittest.TestLoader()
+tests = TL.loadTestsFromModule(SpectraTools.Tests.nist_Tests)
+TS.addTests(tests)
 
-# TL = unittest.TestLoader()
-# tests = TL.loadTestsFromModule(SpectraTools.Tests.RefractiveIndex_Tests)
-# TS.addTests(tests)
+TL = unittest.TestLoader()
+tests = TL.loadTestsFromModule(SpectraTools.Tests.RefractiveIndex_Tests)
+TS.addTests(tests)
 
-# TL = unittest.TestLoader()
-# tests = TL.loadTestsFromModule(SpectraTools.Tests.RI_read_yaml_Tests)
-# TS.addTests(tests)
+TL = unittest.TestLoader()
+tests = TL.loadTestsFromModule(SpectraTools.Tests.RI_read_yaml_Tests)
+TS.addTests(tests)
 
-# TL = unittest.TestLoader()
-# tests = TL.loadTestsFromModule(SpectraTools.Tests.UnitConversion_Tests)
-# TS.addTests(tests)
+TL = unittest.TestLoader()
+tests = TL.loadTestsFromModule(SpectraTools.Tests.UnitConversion_Tests)
+TS.addTests(tests)
 
 result = unittest.TestResult()
 TS.run(result)
